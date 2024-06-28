44348b3ed4eca13f016a3b5a8ae988000fe3bc9d
rojoyin <rojoyin@gmail.com>
2024-06-28 09:24:45 -0500

    Defatult logger

---
+++ logging_playground/__main__.py
@@ -0,0 +1,8 @@
+import logging
+
+# default logging min level is warning
+logging.debug("Debug message")
+logging.info("Info message")
+logging.warning("Warning message")
+logging.error("Error message")
+logging.critical("Critical message")
