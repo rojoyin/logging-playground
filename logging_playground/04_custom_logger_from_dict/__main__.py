08919d8e5bd8fb15f5150a768ae24384041926ea
rojoyin <rojoyin@gmail.com>
2024-06-30 13:04:23 -0500

    Adjust file location for running python module

--- logging_playground/04_custom_logger_from_dict/__main__.py
+++ logging_playground/04_custom_logger_from_dict/__main__.py
@@ -2,7 +2,7 @@ import logging.config
 import yaml
 
 
-with open("config.yml", "r") as f:
+with open("04_custom_logger_from_dict/config.yml", "r") as f:
     config = yaml.safe_load(f.read())
     logging.config.dictConfig(config)
 
