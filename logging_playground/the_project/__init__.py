a77394a7fc53dfc496b5bfa54baa59db791ed0f7
rojoyin <rojoyin@gmail.com>
2024-07-01 16:30:31 -0500

    Configure project logger

---
+++ logging_playground/the_project/__init__.py
@@ -0,0 +1,11 @@
+import logging
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+console_formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+console_handler = logging.StreamHandler()
+
+console_handler.setFormatter(console_formatter)
+
+logger.addHandler(console_handler)
